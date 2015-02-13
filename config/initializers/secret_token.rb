2f515649f28bcf396d013d042344b3ef5445a862
Diego Castillo <castillodie@gmail.com>
2015-02-13 18:18:43 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SistemaVyp::Application.config.secret_token = 'ac661d7087970695d1416a31ffb8adecebd285491b3c5e2db66590fba67e5c5d287122ababee7b5434028c088293e7f469dd5e95ab208c6a39521269244ac174'
