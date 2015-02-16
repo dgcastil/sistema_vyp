0c28f666b99211524cf9d69a5b6d0f6e6ca0fe7e
Diego Castillo <castillodie@gmail.com>
2015-02-16 01:46:25 -0300

    Termino diseño y rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SistemaVyp::Application.routes.draw do
-  get "static_pages/home"
+  
+  get "users/new"
 
-  get "static_pages/help"
+ root to: 'static_pages#home'
+ match '/help',to: 'static_pages#help'
+ match '/signup',to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -49,10 +52,6 @@ SistemaVyp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
