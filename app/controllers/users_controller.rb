1e72e6eae86fb268f1885f9b4cbd7b2232c42446
Diego Castillo <castillodie@gmail.com>
2015-02-18 10:17:45 -0300

    Finalizado usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+	before_filter :signed_in_user, only: [:index,:edit, :update]
+	before_filter :correct_user, only: [:edit, :update]
+	before_filter :admin_user, only: [:destroy]
+
 	def new
 		@user = User.new
 	end
@@ -6,12 +10,36 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 	def create
-	@user = User.new(params[:user])
+		@user = User.new(params[:user])
 		if @user.save
 		flash[:success] = "Usuario registrado"
 		redirect_to @user
 		else
 		render 'new'
+		end
+	end
+
+	def index
+		@users = User.paginate(page: params[:page])
+	end
+
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User destroyed."
+		redirect_to users_path
+	end
+	private
+		def signed_in_user
+			unless signed_in?
+			store_location
+			redirect_to signin_path, notice: "Please sign in." 
+		end
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_path) unless current_user?(@user)
+		end
+		def admin_user
+			redirect_to(root path) unless current_user.admin?
 		end
 	end
 end
