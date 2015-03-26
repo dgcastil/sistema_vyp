3059992996f53069fcb8d41683f1fb8baa653932
Diego Castillo <castillodie@gmail.com>
2015-03-26 20:35:20 -0300

    pdf listo

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -10,30 +10,27 @@ class PdfController < ApplicationController
 
 	def create
 		@order = current_order
-
-	
-
 		session[:order_id]=nil
-	 	# Load the html to convert to PDF
-	   # html = File.read("#{Rails.root}/app/views/pdf/_pdf.html.erb")
 	    html = render_to_string(:layout => false , :template => "pdf/_pdf.html.erb", :formats => :html)
 	    # Create a new kit and define page size to be US letter
 	    kit = PDFKit.new(html, :page_size => 'Letter')
 	    kit.stylesheets << "#{Rails.root}/public/assets/application.css"
+
 	  
 	    
 		#pdf = kit.to_pdf
 
 	    kit.to_file("#{Rails.root}/public/example.pdf")
+	 
 	    send_file "#{Rails.root}/public/example.pdf",
-      		:type => "application/pdf",
-      		:file_name => "example.pdf",
-      		:disable_smart_shrinking => true,
+     		:type => "application/pdf",
+    		:file_name => "example.pdf",
       		:disposition => 'inline',
        		:stream => false,
-       		:lowquality => false,
-       		:disable_pdf_compression=> true
-	
+       		:disable_smart_shrinking=> true,
+    		:disable_pdf_compression=> true,
+    		:dpi =>'300'
+      		
 	end
 
 
--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,5 +1,7 @@
 PDFKit.configure do |config|
+  
   if File.executable? '~/.apt/usr/local/bin/wkhtmltopdf'
     config.wkhtmltopdf = '~/.apt/usr/local/bin/wkhtmltopdf'
   end
+	
 end
